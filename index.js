a2dd966b032d5d2bbbba62f1f9624a6cb2de6a80
Excigma <tweettweettaiwei@gmail.com>
2019-06-02 17:17:06 +1200

    Add timetable to Heroku

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 const express = require("express"),
-	bodyParser = require("body-parser");
+    bodyParser = require("body-parser");
 
 require("dotenv").config();
 
 String.prototype.replaceAll = function (search, replacement) {
-	return this.replace(new RegExp(search, "g"), replacement);
+    return this.replace(new RegExp(search, "g"), replacement);
 };
 
 
 require("./lib/routes")(express()
-	.use(bodyParser.urlencoded({ extended: false }))
-	.use(bodyParser.json())
-	.use(require("compression")())
+    .use(bodyParser.urlencoded({ extended: false }))
+    .use(bodyParser.json())
+    .use(require("compression")())
 
-	.engine(".ejs", require("ejs").__express)
-	.set("view engine", "ejs")
+    .engine(".ejs", require("ejs").__express)
+    .set("view engine", "ejs")
 
-	.set("etag", true)
-	.set("env", "production")
-	.set("view cache", true)
-	.set("views", `./lib/pages`)
-	.set("strict routing", false)
-	.set("x-powered-by", false)
-	.set("case sensitive routing", false)
+    .set("etag", true)
+    .set("env", "production")
+    .set("view cache", true)
+    .set("views", `./lib/pages`)
+    .set("strict routing", false)
+    .set("x-powered-by", false)
+    .set("case sensitive routing", false)
 );
-
